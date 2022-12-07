e36e6839b85037cbb55e52974786386b8cecbcc8
Ori Daniel <ori.daniel2018@gmail.com>
2022-12-07 23:36:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, Button, Box } from "@mui/material";
+import i18n from './i18n'
+
+export default function App() {
+  return (
+    <Box>
+    <Typography variant="h1">Hello</Typography>
+    <Button variant="contained">Test Button</Button>
+    </Box>
+  );
+}
